5a3cdf8a1b7dc2c5b5363c5be4b65ee9c6add5f9
Anh Huynh <elfking@yahoo.com>
2013-11-20 18:01:12 -0800

    small bugs squashed

--- generative.py
+++ generative.py
@@ -3,7 +3,11 @@ Final Project
 """
 
 import numpy as np
+import numpy.linalg as LA
 import matploylib.pyplot as pyplot
+import time
+
+t = time.time()
 sample_size = 20000
 
 #############Read reduced images and labels################
@@ -15,16 +19,30 @@ img_A = np.array(np.load(f_img_A, mmap_mode='r'))
 img_B = np.array(np.load(f_img_B, mmap_mode='r'))
 img_size = len(img_A[0])**2
 
+####Need image as vector
+
 #############Fit Gaussian model#################
 def gaussian_fit(images):
+        """
+                Estimate Gaussian model from data
+
+                images = list of 7x7 flattened images
+        """
 	mean = sum([image for image in images])/(len(images)+0.0)
-	variance = image.T.dot(image)/(len(images)-1.0)
+	variance = image.T.dot(image)*(1/(len(images)-1.0))
 	return (mean, variance)
 
 #############Multivariate normal density########
 def multinormalpdf(muSigma, x):
+        """
+                Return to probability of getting x given multinormal
+                distribution with params mu, Sigma
+
+                input: muSigma = [mu, Sigma]
+        """
         mu, Sigma = muSigma
-        return np.exp(-0.5*(x-mu).T.dot(np.linalg.inv(Sigma)).dot(x-mu))/sqrt(2*np.pi)**img_size/sqrt(np.linalg.det(Sigma))
+        return np.exp(-0.5*(x-mu).T.dot(LA.inv(Sigma)).dot(x-mu))
+        /sqrt(2*np.pi)**img_size/sqrt(LA.det(Sigma))
 
 #############Gradient Descent###################
 def grad_desc(model, init, image):
@@ -36,6 +54,13 @@ def grad_desc(model, init, image):
 
 #############Classifier#########################
 def generative(gauss,image):
+        """
+                Classify according to GDA
+
+                Input:
+                Gaussian params gauss = [[mu1,Sigma1],[mu2,Sigma2],..]
+                image = 7x7 flatten image
+        """
         p_vector = np.array([0.1 for i in range(10)])
         p_vector = grad_desc(gauss, p_vector, image)
         return p_vector.index(max(p_vector))
@@ -50,6 +75,10 @@ for i in range(10):
 	np.append(gauss,[gaussian_fit(batch)])
 
 for i in range(sample_size):
+        """
+                1-0 loss
+        """
 	if generative(gauss,images_B[i])!=label_B[i]:
 		error += 1
 print error/(sample_size+0.0)
+print time.time()-t
