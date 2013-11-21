20d1e8bc5907eb3cf43c784f7c4860635b05e564
chauvm <chauvm@mit.edu>
2013-11-20 19:37:09 -0500

    Add comments

--- generative.py
+++ generative.py
@@ -6,7 +6,7 @@ import numpy as np
 import matploylib.pyplot as pyplot
 sample_size = 20000
 
-#############Read reduced images and labels################
+#############  Read reduced images and labels  ################
 f_img_A = "trainingImagesA_red.npy"
 f_img_B = "trainingImagesB_red.npy"
 f_label_A = "trainingLabelsA.npy"
@@ -15,32 +15,38 @@ img_A = np.array(np.load(f_img_A, mmap_mode='r'))
 img_B = np.array(np.load(f_img_B, mmap_mode='r'))
 img_size = len(img_A[0])**2
 
-#############Fit Gaussian model#################
+############# Fit Gaussian model #################
 def gaussian_fit(images):
 	mean = sum([image for image in images])/(len(images)+0.0)
 	variance = image.T.dot(image)/(len(images)-1.0)
 	return (mean, variance)
 
-#############Multivariate normal density########
+############# Multivariate normal density ########
 def multinormalpdf(muSigma, x):
         mu, Sigma = muSigma
         return np.exp(-0.5*(x-mu).T.dot(np.linalg.inv(Sigma)).dot(x-mu))/sqrt(2*np.pi)**img_size/sqrt(np.linalg.det(Sigma))
 
-#############Gradient Descent###################
+############# Gradient Descent ###################
 def grad_desc(model, init, image):
+        """
+                given (gaussian) model, init prob distribution, return new prob distribution
+        """
         params = init
         while (True):
                 loss = sum([log(multinormalpdf(model[i],image)) for i in range(10)])
                 gradient = gradient(loss,,,)
-                ###careful: we can't make sure probs sum to 1
+                ### careful: we can't make sure probs sum to 1
 
-#############Classifier#########################
+############# Classifier #########################
 def generative(gauss,image):
+        """
+                given the image, return label with max prob
+        """
         p_vector = np.array([0.1 for i in range(10)])
         p_vector = grad_desc(gauss, p_vector, image)
         return p_vector.index(max(p_vector))
 
-#############Execution##########################
+############# Execution ##########################
 gauss = []
 for i in range(10):
 	"""
