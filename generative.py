c65064141a5b3a604e5abe3746b95405115cbb10
chauvm <chauvm@mit.edu>
2013-11-24 15:10:45 -0500

    Before pull flatten data

--- generative.py
+++ generative.py
@@ -19,14 +19,30 @@ img_size = len(img_A[0])**2
 def gaussian_fit(images):
 	mean = sum([image for image in images])/(len(images)+0.0)
 	variance = image.T.dot(image)/(len(images)-1.0)
-	return (mean, variance)
+	print "Variance: ", variance
+	zero_rows = []
+	for i in range(len(variance)):
+		if np.all(variance[i]==0):
+			zero_rows.append(i)
+        new_mean = np.delete(mean, zero_rows)
+        new_variance = np.delete(variance, zero_rows)
+        ind_mean_dict = {}
+        for ind in zero_rows:
+                ind_mean_dict[ind] = mean[ind]
+                print "New variance: ", new_variance
+	return (new_mean, new_variance, ind_mean_dict)
 
 ############# Multivariate normal density ########
-def multinormalpdf(muSigma, x):
-        mu, Sigma = muSigma
-        print "Sigma: ", Sigma
-        denom = np.sqrt(2*np.pi)**img_size/np.sqrt(np.linalg.det(Sigma))
-        return np.exp(-0.5*(x-mu).T.dot(np.linalg.inv(Sigma)).dot(x-mu))/denom
+def multinormalpdf((mu, Sigma, ind_mean_dict), x):
+		#print "Sigma: ", Sigma
+		
+		for ind in ind_mean_dict.keys():
+				if np.all((x[ind] - ind_mean_dict[ind]) == 0):
+						return 0
+				else:
+						new_x = np.delete(x, ind_mean_dict.keys())
+						denom = np.sqrt(2*np.pi)**img_size/np.sqrt(np.linalg.det(Sigma))
+						return np.exp(-0.5*(new_x-mu).T.dot(np.linalg.inv(Sigma)).dot(new_x-mu))/denom
 
 
 #################################
@@ -35,55 +51,55 @@ def multinormalpdf(muSigma, x):
 
 ############# prob_mixture #######################
 def prob_mixture(gausses, p_vector, image):
-    """
-     Likelihood of image drawn from 10 Gaussians with p_vector
-     (think of a better name later)
-    """
-    # log sum of p*Prob(image comes from each Gaussian)
-    return np.log(sum([p_vector[i]*multinormalpdf(gausses[i], image) for i in range(2,10)]))
+	"""
+	 Likelihood of image drawn from 10 Gaussians with p_vector
+	 (think of a better name later)
+	"""
+	# log sum of p*Prob(image comes from each Gaussian)
+	return np.log(sum([p_vector[i]*multinormalpdf(gausses[i], image) for i in range(10)]))
 
 ############# Gradient ###########################
 def gradient(function, gausses, p_vector, image, delta):
-    print "Num. Gausses : ", len(gausses)
-    print "Num. p vector : ", len(p_vector)
-    dims = len(p_vector)
-    grad = np.zeros(dims)  
-    tmp = np.zeros(dims)    
-    for i in xrange(dims):     
-        tmp[i] = delta
-        grad[i] = (function(gausses, (p_vector+ tmp), image) - function(gausses, (p_vector- tmp), image))/delta
-        tmp[i] = 0
-    return grad
-    
+	print "Num. Gausses : ", len(gausses)
+	print "Num. p vector : ", len(p_vector)
+	dims = len(p_vector)
+	grad = np.zeros(dims)  
+	tmp = np.zeros(dims)    
+	for i in xrange(dims):     
+		tmp[i] = delta
+		grad[i] = (function(gausses, (p_vector+ tmp), image) - function(gausses, (p_vector- tmp), image))/delta
+		tmp[i] = 0
+	return grad
+	
 ############# Normalize p_vector #################
 def normalize_pvector(p_vector, gradient, delta):
-    """
-        Given p_vector and its gradient, return new p_vector with sum(p_vector) = 1
-    """
-    new_pvector = p_vector + gradient*delta
-    return new_pvector/sum(new_pvector)
+	"""
+		Given p_vector and its gradient, return new p_vector with sum(p_vector) = 1
+	"""
+	new_pvector = p_vector + gradient*delta
+	return new_pvector/sum(new_pvector)
 ############# Gradient Descent ###################
 def grad_desc(model, init, image):
-        """
-                given (gaussian) model, init prob distribution, return new prob distribution
-        """
-        params = init
-        delta = 0.005
-        acceptance = 0.01
-        while (True):
-                ## loss = sum of log likelihood of G(i, image)    
-                # why you called it 'loss'? sum of log or log of sum
-                #loss = sum([log(multinormalpdf(model[i],image)) for i in range(10)])
-                # gradient should return gradients for 10 p's
-                grad = gradient(prob_mixture, model, params, image, delta)
-                new_params = normalize_pvector(params, grad, delta)
-                
-                # make use of dist function, if params do not change significantly, stop iteration
-                if dist(new_params, params) < acceptance:
-                    break
-                else:
-                    params = new_params
-        return new_params
+		"""
+				given (gaussian) model, init prob distribution, return new prob distribution
+		"""
+		params = init
+		delta = 0.005
+		acceptance = 0.01
+		while (True):
+				## loss = sum of log likelihood of G(i, image)    
+				# why you called it 'loss'? sum of log or log of sum
+				#loss = sum([log(multinormalpdf(model[i],image)) for i in range(10)])
+				# gradient should return gradients for 10 p's
+				grad = gradient(prob_mixture, model, params, image, delta)
+				new_params = normalize_pvector(params, grad, delta)
+				
+				# make use of dist function, if params do not change significantly, stop iteration
+				if dist(new_params, params) < acceptance:
+					break
+				else:
+					params = new_params
+		return new_params
 
 #################################
 ################  End of 11/20
@@ -91,12 +107,12 @@ def grad_desc(model, init, image):
 
 ############# Classifier #########################
 def generative(gauss,image):
-        """
-                given the image, return label with max prob
-        """
-        p_vector = np.array([0.1 for i in range(10)])
-        p_vector = grad_desc(gauss, p_vector, image)
-        return p_vector.index(max(p_vector))
+		"""
+				given the image, return label with max prob
+		"""
+		p_vector = np.array([0.1 for i in range(10)])
+		p_vector = grad_desc(gauss, p_vector, image)
+		return p_vector.index(max(p_vector))
 
 ############# Execution ##########################
 gauss = []
