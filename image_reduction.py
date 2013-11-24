ba2d8e9dd45731a5f5e48f0cdeabb5901e9506ec
chauvm <chauvm@mit.edu>
2013-11-24 18:39:48 -0500

    Reduce image to 49 by 1

--- image_reduction.py
+++ image_reduction.py
@@ -20,7 +20,7 @@ img_A = np.array(np.load(f_img_A, mmap_mode='r'))
 f_img_B = "trainingImagesB.npy"
 img_B = np.array(np.load(f_img_B, mmap_mode='r'))
 
-img_red_A = np.array([ normalize(img_A[i]) for i in range(size_data)])
-img_red_B = np.array([ normalize(img_B[i]) for i in range(size_data)])
+img_red_A = np.array([ normalize(img_A[i]).flatten() for i in range(size_data)])
+img_red_B = np.array([ normalize(img_B[i]).flatten() for i in range(size_data)])
 np.save("trainingImagesA_reduced.npy",img_red_A)
 np.save("trainingImagesB_reduced.npy",img_red_B)
